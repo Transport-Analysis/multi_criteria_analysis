f0ac78e45cf030b8482d5a9892223bb17c498742
Pete Stephan <peter.stephan@pwc.com>
2022-06-23 21:33:22 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -265,28 +265,28 @@ with st.expander("Criteria", expanded=False):
 AvailableRanks = list(range(1,len(UserInputs) + 1))
 with st.expander("Criteria Weighting and Scoring", expanded=False):
 for Criterion, row in UserInputs.iterrows():
-    with st.expander("Criterion: %s" % Criterion):
-        label = 'Rank - criterion: %s' % Criterion
-        index = AvailableRanks.index(row.Ranks) if row.Ranks in AvailableRanks else 0
-        col1,col2,col3,col4 = st.columns([1.5,1,1,1])
-        with col1:         
-            UserInputs.at[Criterion, 'Ranks'] = st.selectbox(label, AvailableRanks, index, key='rank_%s' % Criterion)
-        col11,col22,col33 = st.columns(3)
-        i=1
-        for OptionName in UserInputs.columns[i:][~UserInputs.columns[i:].isin(Updated_Input)]:
-            value = UserInputs.at[Criterion, OptionName]
-            key = 'scores_%s_%s' % (Criterion, OptionName)
-            if i==1 or i%3==1:
-                with col11:
-                    UserInputs.at[Criterion, OptionName] = st.select_slider('Score - option: %s' %  OptionName, range(1,6), key=key, value=value)
-            elif i==2 or i%3==2:
-                with col22:
-                    UserInputs.at[Criterion, OptionName] = st.select_slider('Score - option: %s' %  OptionName, range(1,6), key=key, value=value)
-            else:
-                with col33:
-                    UserInputs.at[Criterion, OptionName] = st.select_slider('Score - option: %s' %  OptionName, range(1,6), key=key, value=value)                 
-            c1, c2, c3, c4 = slider_colour(value)
-            i+=1
+    st.write("Criterion: %s" % Criterion):
+    label = 'Rank - criterion: %s' % Criterion
+    index = AvailableRanks.index(row.Ranks) if row.Ranks in AvailableRanks else 0
+    col1,col2,col3,col4 = st.columns([1.5,1,1,1])
+    with col1:         
+        UserInputs.at[Criterion, 'Ranks'] = st.selectbox(label, AvailableRanks, index, key='rank_%s' % Criterion)
+    col11,col22,col33 = st.columns(3)
+    i=1
+    for OptionName in UserInputs.columns[i:][~UserInputs.columns[i:].isin(Updated_Input)]:
+        value = UserInputs.at[Criterion, OptionName]
+        key = 'scores_%s_%s' % (Criterion, OptionName)
+        if i==1 or i%3==1
+            with col11:
+                UserInputs.at[Criterion, OptionName] = st.select_slider('Score - option: %s' %  OptionName, range(1,6), key=key, value=value)
+        elif i==2 or i%3==2:
+            with col22:
+                UserInputs.at[Criterion, OptionName] = st.select_slider('Score - option: %s' %  OptionName, range(1,6), key=key, value=value
+        else:
+            with col33:
+                UserInputs.at[Criterion, OptionName] = st.select_slider('Score - option: %s' %  OptionName, range(1,6), key=key, value=value)                 
+        c1, c2, c3, c4 = slider_colour(value)
+        i+=1
     used = int(np.where(UserInputs.index.to_numpy() == Criterion)[0])
     AvailableRanks = [x for x in AvailableRanks if x not in list(UserInputs.Ranks.to_numpy())[:used+1]]
     
